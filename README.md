a220d4bfec4c8500f65a7ad70d67e3667a6b1bac
JAVIER MARTIN PIZARRO <jmartinpizarro04@gmail.com>
2025-03-22 10:49:51 +0100

    feat: connection statics files, express and sockets

--- README.md
+++ README.md
@@ -14,3 +14,11 @@ Usage of:
 - Socket.io - [docs](https://socket.io/docs/v4/server-initialization/)
 
 As when doing `node .` we will be executing the backend, the entrance file is `server.js`
+
+## The project
+
+```bash
+node .
+```
+
+Then go to `localhost:3000` to start being served with the statics pages of the project.
--- src/scripts/server.js
+++ src/scripts/server.js
@@ -1,13 +1,34 @@
 const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
+const path = require("path");
 
 const app = express();
 const httpServer = createServer(app);
-const io = new Server(httpServer, { /* options */ });
+const io = new Server(httpServer);
 
-io.on("connection", (socket) => {
-  // ...
+// route defining for loading index.html at localhost:3000
+const PAGES_ROUTE = path.join(__dirname, "../..", "src", "pages");
+
+app.get("/", (req, res) => { // GET: runs the index page
+  res.sendFile(path.join(PAGES_ROUTE, 'index.html'));
 });
 
-httpServer.listen(3000);
+// Web socket connections
+io.on("connection", (socket) => {
+  console.log("🔌 Cliente conectado:", socket.id);
+
+  socket.on("mensaje", (data) => {
+    console.log("📩 Mensaje recibido:", data);
+    io.emit("mensaje", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("❌ Cliente desconectado:", socket.id);
+  });
+});
+
+const PORT = 3000;
+httpServer.listen(PORT, () => {
+  console.log(`✅ Servidor corriendo en http://localhost:${PORT}`);
+});
